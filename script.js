c1b437b0742052f3969aa488e9f4a93e846341b9
Ali <ali@tomorrowagency.com>
2023-08-14 08:34:26 +0100

    fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let INITIAL_DATA_PATH = 'data.json',
-  container = document.querySelector('[data-main-container]'),
+  STORAGE_KEY;
+
+const container = document.querySelector('[data-main-container]'),
   buttonNext = document.querySelector('[data-next-button]'),
   buttonPrev = document.querySelector('[data-prev-button]'),
   cardCounter = document.querySelector('[data-card-counter]'),
@@ -79,30 +81,42 @@ const initMenu = () => {
 const initDeleteItems = () => {
   const deleteItemButtons = document.querySelectorAll('[data-delete-card]');
   deleteItemButtons.forEach(button => button.addEventListener('click', ()=> {
-    const newArr = JSON.parse(localStorage.getItem('words')).filter(el => el.english !== button.dataset.deleteCard);
-    localStorage.setItem('words', JSON.stringify(newArr));
+    const newArr = JSON.parse(localStorage.getItem(STORAGE_KEY)).filter(el => el.english !== button.dataset.deleteCard);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(newArr));
     generateWordCards();
   }));
 }
 
-async function generateWordCards(type) {
+async function generateWordCards(type = 'data') {
   let storedWords;
   let data;
+  STORAGE_KEY = type;
 
-  if (type && type !== 'initial') {
+  if (type) {
     INITIAL_DATA_PATH = `${type}.json`,
     storedWords = JSON.parse(localStorage.getItem(type)) || [];
     data = !storedWords.length ? await getData() : storedWords;
     !storedWords.length && localStorage.setItem(type, JSON.stringify(data));
   } else {
-    storedWords = JSON.parse(localStorage.getItem('words')) || [];
+    storedWords = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 
     data = !storedWords.length ? await getData() : storedWords;
-    !storedWords.length && localStorage.setItem('words', JSON.stringify(data));
+    !storedWords.length && localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
   }
 
   container.scrollLeft = 0;
   let html = '';
+
+  const deleteButton = (card) => {
+    return INITIAL_DATA_PATH === 'data.json'
+     ? (`
+      <button class="button button--icon" type="button" data-delete-card="${card.english}">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+        </svg>
+      </button>
+     `) : '';
+  }
 
   data.forEach(card => {
     const singleCard = `
@@ -111,11 +125,7 @@ async function generateWordCards(type) {
         <div class="card__front">
           <h4>${card.english}</h4>
           <button class="button button--voice-output" type="button" data-generate-en-voice-output="${card.english}">say</button>
-          <button class="button button--icon" type="button" data-delete-card="${card.english}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-            </svg>
-          </button>
+          ${deleteButton(card)}
         </div>
         <div class="card__back">
           <h4>${card.russian}</h4>
@@ -147,13 +157,13 @@ const initFormAddWord = () => {
 
     if (newWord.trim() === "" || translation.trim() === "") return;
 
-    localStorage.setItem('words', JSON.stringify([
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([
       {
         "id": `${newWord.trim()}`,
         "english": `${newWord.trim()}`,
         "russian": `${translation.trim()}`
       },
-      ...JSON.parse(localStorage.getItem('words'))
+      ...JSON.parse(localStorage.getItem(STORAGE_KEY))
     ]));
 
     e.target.elements["add-word"].value = "";
